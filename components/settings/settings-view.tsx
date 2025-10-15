fd46000b7dcf2d977b94a4499d326f0635f5bf67
EreQ <info.ericmail@gmail.com>
2025-10-15 13:36:48 +0000

    use client

--- components/settings/settings-view.tsx
+++ components/settings/settings-view.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Suspense } from "react"
 import { Settings } from "@/components/settings/components/settings"
 import { SettingsSkeleton } from "@/components/settings/components/settings-skeleton"
